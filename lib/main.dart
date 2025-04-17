646ce9375162dbfec2714cda6c5a3ad3304b1f18
muhammed sinan <sinanzzsinu70@gmail.com>
2025-04-17 14:35:28 +0530

    Firebase successfully configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:web_view_app/firebase_options.dart';
 import 'package:web_view_app/presentation/bloc/login_page/login_page_bloc.dart';
 import 'package:web_view_app/presentation/bloc/otp_page/otp_page_bloc.dart';
 import 'package:web_view_app/presentation/pages/login_screen/login_page.dart';
 
-void main() {
+void main()async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
