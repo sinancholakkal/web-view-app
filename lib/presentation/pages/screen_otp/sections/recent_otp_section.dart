a76005f004a47906b7ee03d3ee30f36a4c5b0956
muhammed sinan <sinanzzsinu70@gmail.com>
2025-04-18 11:15:09 +0530

    OTP timer locally added

--- lib/presentation/pages/screen_otp/sections/recent_otp_section.dart
+++ lib/presentation/pages/screen_otp/sections/recent_otp_section.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:otp_resend_timer/otp_resend_timer.dart';
 import 'package:web_view_app/core/constent.dart';
 import 'package:web_view_app/widget/text_widget.dart';
 
 class RecentOtpSection extends StatelessWidget {
-  const RecentOtpSection({
+  RecentOtpSection({
     super.key,
   });
+  final controller = OtpResendTimerController(initialTime: 60);
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +16,34 @@ class RecentOtpSection extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           sizedBoxH50,
-    Center(
-        child: TextWidget(
-      text: "Didn't you receive any code?",
-      color: Colors.red[400],
-      size: 15,
-    )),
-    Center(
-        child: TextButton(
-      onPressed: () {},
-      child: const TextWidget(
-        text: "RESEND NEW CODE",
-        color: Colors.red,
-        size: 15,
-      ),
-    ))
+          Center(
+              child: TextWidget(
+            text: "Didn't you receive any code?",
+            color: Colors.red[400],
+            size: 15,
+          )),
+          sizedBoxH10,
+          OtpResendTimer(
+              controller: controller,
+
+              readyMessageStyle: TextStyle(
+                fontSize: 14
+              ),
+              autoStart: true,
+              timerMessage: "Resend OTP in ",
+              readyMessage: "You can now resend the code",
+              onFinish: () {},
+              onResendClicked: () {},
+              onStart: () {}),
+          // Center(
+          //     child: TextButton(
+          //   onPressed: () {},
+          //   child: const TextWidget(
+          //     text: "RESEND NEW CODE",
+          //     color: Colors.red,
+          //     size: 15,
+          //   ),
+          // ))
         ],
       ),
     );
